0521af90d24862a7ccf0c7ea7d17b474795d5e40
henry chart <henry.chart@starcount.com>
2018-04-06 15:51:45 +0100

    updated version

--- cars/spiders/main_generate_links.py
+++ cars/spiders/main_generate_links.py
@@ -9,7 +9,6 @@ class SearchCarSpider(BaseSpider):
         self.crawler.stats.inc_value('car/search/request')
         # Extract urls
         links = response.xpath('//div[@class="listing-headline"]/a[@href]/@href').extract()
-        print links
         for link in links:
             #yield #self.base_url + link #(workout how to yeild the results)
             yield scrapy.Request(url=self.base_url + link, callback=self.extract_results, errback=self.process_search_error)
@@ -22,7 +21,22 @@ class SearchCarSpider(BaseSpider):
             'result_url': None
         }
 
+
+
     def extract_results(self, response):
-        print response.url
+        price = response.xpath('//span[@class="item--price"]/text()').extract()
+        engine_litre = response.xpath('//ul[@class="feature-specs"]//p[@xpath = "1"]/text()')#.extract()
+        mileage = response.xpath('//ul[@class="feature-specs"]//p[@xpath="2"]/text()').extract()
+        bhp = response.xpath('//ul[@class="feature-specs"]//p[@xpath="3"]/text()').extract()
+        fuel = response.xpath('//ul[@class="feature-specs"]//p[@xpath="4"]/text()').extract()
+        gearbox = response.xpath('//ul[@class="feature-specs"]//p[@xpath="5"]/text()').extract()
+        print price
+        print engine_litre
+        print mileage
+        print bhp
+        print fuel
+        print gearbox
+
+    # Vechile description
 
 
