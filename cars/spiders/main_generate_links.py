9243032ebc8d74693fe365ec5cfc5a30a348b55c
henry chart <henry.chart@starcount.com>
2018-04-12 17:58:59 +0100

    update

--- cars/spiders/main_generate_links.py
+++ cars/spiders/main_generate_links.py
@@ -10,7 +10,6 @@ class SearchCarSpider(BaseSpider):
         # Extract urls
         links = response.xpath('//div[@class="listing-headline"]/a[@href]/@href').extract()
         for link in links:
-            #yield #self.base_url + link #(workout how to yeild the results)
             yield scrapy.Request(url=self.base_url + link, callback=self.extract_results, errback=self.process_search_error)
 
     def process_search_error(self, failure):
@@ -25,12 +24,11 @@ class SearchCarSpider(BaseSpider):
     def extract_results(self, response):
         price = response.xpath('//span[@class="item--price"]/text()').extract_first()
         features = response.xpath('//ul[@class="feature-specs"]')
-        print dict(zip(['Engine_Litre','Mileage','BHP','Fuel','Manual','Price'], [f.extract() for f in features.xpath('.//p/text()')] + [price]))
+        desc = response.xpath('//section[@class="advert-description"]/text()').extract_first()
+        title = response.xpath('//h1[@class="main--title grid__item three-quarters"]/text()').extract_first()
+        yield dict(zip(['Engine_Litre','Mileage','BHP','Fuel','Manual','Price', 'Desc', 'Title'], [f.extract() for f in features.xpath('.//p/text()')] + [price] + [desc] + [title]))
 
-
-
-    # Vechile description
-    # Vechile title
-    # Handiling next page
+        response.xpath('//li[@class=" next"]/a[@href]').extract_first()
+    # Handiling next page yield a request to go to next page
 
 
