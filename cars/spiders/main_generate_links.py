5b722757c4263af08811a7b360ce97173ab0d8c2
henry chart <henry.chart@starcount.com>
2018-04-17 17:39:10 +0100

    updates

--- cars/spiders/main_generate_links.py
+++ cars/spiders/main_generate_links.py
@@ -3,6 +3,7 @@ import scrapy
 
 class SearchCarSpider(BaseSpider):
 
+    relative_url = 'https://www.pistonheads.com/'
     name = 'search_car_spider'
 
     def parse_search_results(self, response):
@@ -12,6 +13,11 @@ class SearchCarSpider(BaseSpider):
         for link in links:
             yield scrapy.Request(url=self.base_url + link, callback=self.extract_results, errback=self.process_search_error)
 
+        relative_next_url = response.xpath('//li[@class=" next"]/a[@href]/@href').extract_first()
+        absolute_next_url = '{}{}'.format(self.base_url, relative_next_url)
+        if absolute_next_url:
+            yield scrapy.Request(absolute_next_url, callback=self.parse_search_results)
+
     def process_search_error(self, failure):
         self.crawler.stats.inc_value('car/search/request')
         yield {
@@ -20,15 +26,9 @@ class SearchCarSpider(BaseSpider):
             'result_url': None
         }
 
-
     def extract_results(self, response):
         price = response.xpath('//span[@class="item--price"]/text()').extract_first()
         features = response.xpath('//ul[@class="feature-specs"]')
         desc = response.xpath('//section[@class="advert-description"]/text()').extract_first()
         title = response.xpath('//h1[@class="main--title grid__item three-quarters"]/text()').extract_first()
         yield dict(zip(['Engine_Litre','Mileage','BHP','Fuel','Manual','Price', 'Desc', 'Title'], [f.extract() for f in features.xpath('.//p/text()')] + [price] + [desc] + [title]))
-
-        response.xpath('//li[@class=" next"]/a[@href]').extract_first()
-    # Handiling next page yield a request to go to next page
-
-
